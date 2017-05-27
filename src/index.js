5d720ebec7b0ee443ce6b7c14be8e93a86f8e2d0
jesuscasesl <jesus.castillo.eslava@gmail.com>
2017-05-27 03:16:19 +0200

    package and index files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
 import App from './Components/App';
 
+import configureStore from './Components/Store/configureStore';
+import initialState from './Components/Reducers/initialState';
+
+const store = configureStore(initialState);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root'),
 );
